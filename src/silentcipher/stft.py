b15d5238a0c621d0b1e642467c404972659e4053
gukush <s184339@student.pg.edu.pl>
2024-10-05 17:46:55 +0200

    Make SilentCipher compatible with PyTorch 2.4.1 for inference

--- src/silentcipher/stft.py
+++ src/silentcipher/stft.py
@@ -20,19 +20,19 @@ class STFT(torch.nn.Module, metaclass=Singleton):
     def transform(self, x):
         x = torch.nn.functional.pad(x, (0, self.win_len - x.shape[1]%self.win_len))
         fft = torch.stft(x, self.filter_length, self.hop_len, self.win_len, window=self.window.to(x.device), return_complex=True)
-    
+
         real_part, imag_part = fft.real, fft.imag
-        
+
         squared = real_part**2 + imag_part**2
         additive_epsilon = torch.ones_like(squared) * (squared == 0).float() * 1e-24
         magnitude = torch.sqrt(squared + additive_epsilon) - torch.sqrt(additive_epsilon)
-        
-        phase = torch.autograd.Variable(torch.atan2(imag_part.data, real_part.data)).float()
+
+        phase = torch.atan2(imag_part.data, real_part.data).float()
         return magnitude, phase
 
     def inverse(self, magnitude, phase):
-        
-        recombine_magnitude_phase = magnitude*torch.cos(phase) + 1j*magnitude*torch.sin(phase)
+
+        recombine_magnitude_phase = torch.complex(magnitude*torch.cos(phase),magnitude*torch.sin(phase))
         inverse_transform = torch.istft(recombine_magnitude_phase, self.filter_length, hop_length=self.hop_len, win_length=self.win_len, window=self.window.to(magnitude.device)).unsqueeze(1)  # , length=self.num_samples
         padding = self.win_len - (self.num_samples % self.win_len)
         inverse_transform = inverse_transform[:, :, :-padding]
